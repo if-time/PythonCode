93b38a43f0dc457ce8e1cc4491c9c05d627fd176
if-time <if-time@users.noreply.github.com>
2019-02-04 13:17:30 +0800

    two

--- PythonStudy/odd.py
+++ PythonStudy/odd.py
@@ -1,11 +1,20 @@
 from datetime import datetime
+from os import getcwd
+import time
+import random
 
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27,29, 31, 33,
         35, 37,39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
 right_this_minute = datetime.today().minute
 
-if right_this_minute in odds:
-    print("This minute seems a little odd.")
-else:
-    print("Not an odd minute")
+for i in range(5):
+    if right_this_minute in odds:
+        print("This minute seems a little odd.")
+        print(getcwd())
+    else:
+        print("Not an odd minute")
+        print(getcwd())
+
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
