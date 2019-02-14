56fa539075552ba055140b137528c4cb94f371c0
if-time <if-time@users.noreply.github.com>
2019-02-14 11:28:13 +0800

    webapp

--- PythonStudy/webapp/vsearchweb.py
+++ PythonStudy/webapp/vsearchweb.py
@@ -1,20 +1,34 @@
-from flask import Flask, render_template, request, redirect, escape
+from flask import Flask, render_template, request, redirect, escape, session
 from vsearch import searchletters
 import mysql.connector
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError
+from checker import check_logged_in
+from threading import Thread
 
 app = Flask(__name__)
 app.config['dbconfig'] = {'host': '127.0.0.1',
-                        'user': 'vsearch',
-                        'password': 'vsearchpasswd',
-                        'database': 'vsearchlogDB', }
-
+                          'user': 'vsearch',
+                          'password': 'vsearchpasswd',
+                          'database': 'vsearchlogDB', }
+app.secret_key = '1234567890'
 # @app.route('/')
 # def hello() -> '302':
 #     return redirect('/entry')
 
 
-def log_request(req: 'flask_request', res: str) ->  None:
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in.'
+
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out'
+
+
+def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
         # print(req, res, file=log)
         # print(str(dir(req)), res, file=log)
@@ -24,31 +38,36 @@ def log_request(req: 'flask_request', res: str) ->  None:
         # print(res, file=log)
         print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
-def log_request_db(req: 'flask_request', res: str) ->  None:
+
+def log_request_db(req: 'flask_request', res: str) -> None:
     """Log details of the web request and the results"""
     dbconfig = {'host': '127.0.0.1',
-		'user': 'vsearch',
-		'password': 'vsearchpasswd',
-		'database': 'vsearchlogDB', }
+                'user': 'vsearch',
+                'password': 'vsearchpasswd',
+                'database': 'vsearchlogDB', }
     conn = mysql.connector.connect(**dbconfig)
     cursor = conn.cursor()
     _SQL = """insert into log
             (phrase, letters, ip, browser_string, results)
             values
             (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res, ))
+    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'],
+                          req.remote_addr, req.user_agent.browser, res, ))
     conn.commit()
     cursor.close()
     conn.close()
 
-def log_request_db_with(req: 'flask_request', res: str) ->  None:
-    
+
+def log_request_db_with(req: 'flask_request', res: str) -> None:
+    raise Exception("Something awful just happened.")
     with UseDatabase(app.config['dbconfig']) as cursor:
-         _SQL = """insert into log
+        _SQL = """insert into log
                 (phrase, letters, ip, browser_string, results)
                 values
                 (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res, ))
+    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'],
+                          req.remote_addr, req.user_agent.browser, res, ))
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -56,9 +75,13 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results'
     results = str(searchletters(phrase, letters))
-    log_request(request, results)
+    try:
+        log_request(request, results)
+    except Exception as err:
+        print('*****Logging failed with this error: ', str(err))
     log_request_db(request, results)
-    return render_template('results.html', the_phrase = phrase, the_letters = letters, the_result = results, the_title = title)
+    log_request_db_with(request, results)
+    return render_template('results.html', the_phrase=phrase, the_letters=letters, the_result=results, the_title=title)
 
 
 @app.route('/')
@@ -68,6 +91,7 @@ def entry_page():
 
 
 @app.route('/viewlog')
+@check_logged_in
 def view_the_log() -> 'html':
     # contents = []
     # with open('vsearch.log') as log:
@@ -77,16 +101,20 @@ def view_the_log() -> 'html':
     #             contents[-1].append(escape(item))
     # titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
     """Display the contents of the log file as a HTML table."""
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-    # return str(contents)
-    return render_template('viewlog.html', 
-                            the_title='View log', 
-                            the_row_titles=titles, 
-                            the_data=contents,)
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        # return str(contents)
+        return render_template('viewlog.html',
+                               the_title='View log',
+                               the_row_titles=titles,
+                               the_data=contents,)
+    except ConnectionError as err:
+        print("Is your database switched on? Error: ", str(err))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
